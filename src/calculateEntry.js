ed37838e3641f6e40818036ed371d86de571d498
Lucas Lisboa <lucas@pop-os.localdomain>
2022-01-06 15:14:56 -0300

    requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,25 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const clientes = { child: 0, adult: 0, senior: 0 };
+  entrants.forEach((atual) => {
+    if (atual.age < 18) clientes.child += 1;
+    if (atual.age >= 18 && atual.age < 50) clientes.adult += 1;
+    if (atual.age >= 50) clientes.senior += 1;
+  });
+  return clientes;
 }
 
 function calculateEntry(entrants) {
-  // seu código aquis
+  if (!entrants || Object.keys(entrants).length === 0) return 0;
+
+  const clientes = countEntrants(entrants);
+  let total = 0;
+  const keys = Object.keys(clientes);
+  keys.forEach((atual) => {
+    total += clientes[atual] * data.prices[atual];
+  });
+  return total;
 }
 
 module.exports = { calculateEntry, countEntrants };
