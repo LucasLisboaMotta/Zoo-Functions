b738d08ff8f38089c1485d7501a736cd17a9db12
Lucas Lisboa <lucas@pop-os.localdomain>
2022-01-06 15:22:06 -0300

    requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const animalList = data.employees.find((actual) => actual.id === id);
+  const firstAnimal = data.species.find((find) => find.id === animalList.responsibleFor[0]);
+  firstAnimal.residents.sort((a, b) => b.age - a.age);
+  return Object.values(firstAnimal.residents[0]);
 }
 
 module.exports = getOldestFromFirstSpecies;
