c181a129b8ec6cc8aa3c4f13a50109615edfffba
Lucas Lisboa <lucas@pop-os.localdomain>
2022-01-06 15:23:48 -0300

    requisito 10

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,62 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const employees = (id) => {
+  const person = data.employees.find((find) => find.id === id);
+  const fullName = `${person.firstName} ${person.lastName}`;
+  const species = person.responsibleFor.reduce((acc, actual) => {
+    const animal = data.species.find((find) => find.id === actual);
+    acc.push(animal.name);
+    return acc;
+  }, []);
+  const locations = species.reduce((acc, actual) => {
+    const location = data.species.find((find) => find.name === actual);
+    acc.push(location.location);
+    return acc;
+  }, []);
+  return {
+    id,
+    fullName,
+    species,
+    locations,
+  };
+};
+
+const allEmployees = () => {
+  const all = data.employees.reduce((acc, actual) => {
+    acc.push(actual.id);
+    return acc;
+  }, []);
+  return all.reduce((acc, actual) => {
+    acc.push(employees(actual));
+    return acc;
+  }, []);
+};
+
+const objectId = (id) => {
+  const finalObject = { id: 'none', ok: false };
+  if (data.employees.find((find) => find.id === id.id)) {
+    finalObject.id = id.id;
+    finalObject.ok = true;
+  }
+  if (data.employees.find((find) => find.firstName === id.name)) {
+    const firstName = data.employees.find((find) => find.firstName === id.name);
+    finalObject.id = firstName.id;
+    finalObject.ok = true;
+  }
+  if (data.employees.find((find) => find.lastName === id.name)) {
+    const lastName = data.employees.find((find) => find.lastName === id.name);
+    finalObject.id = lastName.id;
+    finalObject.ok = true;
+  }
+  return finalObject;
+};
+
+function getEmployeesCoverage(id) {
+  if (!id) return allEmployees();
+  if (data.employees.find((find) => find.id === id)) return employees(id);
+  const trueID = objectId(id);
+  if (trueID.ok) return employees(trueID.id);
+  throw new Error('Informações inválidas');
 }
 
 module.exports = getEmployeesCoverage;
