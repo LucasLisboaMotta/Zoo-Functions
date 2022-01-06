13127ed721a76dc19cb41ffa659333ae082e7e42
Lucas Lisboa <lucas@pop-os.localdomain>
2022-01-06 15:09:01 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    const listaGeral = {};
+    data.species.forEach((valorAtual) => {
+      listaGeral[valorAtual.name] = valorAtual.residents.length;
+    });
+    return listaGeral;
+  }
+
+  if (!animal.sex) {
+    const animais = data.species.find((atual) => atual.name === animal.specie);
+    return animais.residents.length;
+  }
+
+  const animais = data.species.find((atual) => atual.name === animal.specie);
+  return animais.residents.filter((atual) => atual.sex === animal.sex).length;
 }
 
 module.exports = countAnimals;
