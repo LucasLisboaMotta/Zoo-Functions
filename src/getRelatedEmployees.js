fe40adbf6f43472011381e6eeab4e863e2195a85
Lucas Lisboa <lucas@pop-os.localdomain>
2022-01-06 15:03:10 -0300

    requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,24 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  let retorno = false;
+  data.employees.forEach((valorAtual) => {
+    if (valorAtual.managers.find((valor) => valor === id)) retorno = true;
+  });
+  return retorno;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const colaboradores = data.employees.reduce((acumulado, atual) => {
+    if ((atual.managers.find((valor) => valor === managerId))) {
+      acumulado.push(`${atual.firstName} ${atual.lastName}`);
+    }
+    return acumulado;
+  }, []);
+  return colaboradores;
 }
 
 module.exports = { isManager, getRelatedEmployees };
