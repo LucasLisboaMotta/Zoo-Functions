e0086647fc13f11eb5163b091cd1c16948ee2073
Lucas Lisboa <lucas@pop-os.localdomain>
2022-01-06 14:53:58 -0300

    requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const especie = data.species.find((atual) => animal === atual.name);
+  return especie.residents.every((atual) => atual.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
