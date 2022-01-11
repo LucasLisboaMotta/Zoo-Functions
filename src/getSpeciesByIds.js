f7933041564cded89e75e03fc0799c5bac750300
Lucas Lisboa <lucas@pop-os.localdomain>
2022-01-11 11:15:42 -0300

    refatorando getSpecieByIds

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,10 +1,5 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function getSpeciesByIds(...ids) {
-  return ids.reduce((acumulado, atual) => {
-    acumulado.push(data.species.find((atualFind) => atualFind.id === atual));
-    return acumulado;
-  }, []);
-}
+const getSpeciesByIds = (...ids) => ids.map((atual) => species.find(({ id }) => id === atual));
 
 module.exports = getSpeciesByIds;
