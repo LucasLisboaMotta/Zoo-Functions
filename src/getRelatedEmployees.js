dba01c398ee530877e0cec7a1f37d1b6cf82b1c2
Lucas Lisboa <lucas@pop-os.localdomain>
2022-01-11 12:29:51 -0300

    refatorando getRelatedEmployyes

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,24 +1,14 @@
-const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
-function isManager(id) {
-  let retorno = false;
-  data.employees.forEach((valorAtual) => {
-    if (valorAtual.managers.find((valor) => valor === id)) retorno = true;
-  });
-  return retorno;
-}
+const isManager = (id) => employees.some(({ managers }) => managers.includes(id));
 
-function getRelatedEmployees(managerId) {
-  if (!isManager(managerId)) {
-    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
-  }
-  const colaboradores = data.employees.reduce((acumulado, atual) => {
-    if ((atual.managers.find((valor) => valor === managerId))) {
-      acumulado.push(`${atual.firstName} ${atual.lastName}`);
-    }
-    return acumulado;
+const getRelatedEmployees = (id) => {
+  if (!isManager(id)) throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+
+  return employees.reduce((acumulador, { managers, firstName, lastName }) => {
+    if (managers.includes(id)) acumulador.push(`${firstName} ${lastName}`);
+    return acumulador;
   }, []);
-  return colaboradores;
 }
 
 module.exports = { isManager, getRelatedEmployees };
